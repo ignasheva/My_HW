a2267cd3ad186caaf42df1a2e8dada2c041b1ccf
ignasheva <irina.ignasheva.96@mail.ru>
2021-06-26 16:38:19 +0300

    fix bugs

--- tests/homework1/test_task3.py
+++ tests/homework1/test_task3.py
@@ -2,5 +2,5 @@ import pytest
 from homework1.task3 import find_maximum_and_minimum
 
 
-def check_work():
-    assert find_maximum_and_minimum('some_file.txt')
+def test_check_work():
+    assert find_maximum_and_minimum('homework1/some_file.txt')
