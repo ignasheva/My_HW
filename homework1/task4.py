58dc1103d9f7b87465069a2348bba943f91bc961
ignasheva <irina.ignasheva.96@mail.ru>
2021-07-14 21:51:37 +0300

    black

--- homework1/task4.py
+++ homework1/task4.py
@@ -4,4 +4,10 @@ from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
-    return len([Counter(combination) for combination in itertools.product(a, b, c, d) if sum(combination) == 0])
+    return len(
+        [
+            Counter(combination)
+            for combination in itertools.product(a, b, c, d)
+            if sum(combination) == 0
+        ]
+    )
--- homework1/task5.py
+++ homework1/task5.py
@@ -3,7 +3,12 @@ from typing import List
 
 def find_maximal_sub_array_sum(nums: List[int], k: int) -> int:
     variants = []
-    [variants.append(nums[j:i]) for i in range(len(nums) + 1) for j in range(i) if len(nums[j:i]) <= k]
+    list(
+        variants.append(nums[j:i])
+        for i in range(len(nums) + 1)
+        for j in range(i)
+        if len(nums[j:i]) <= k
+    )
     sum_array = []
     [sum_array.append(sum(i)) for i in variants]
     return max(sum_array)
